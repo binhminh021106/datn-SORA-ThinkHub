60b5f6287e160a1aaba0625f78d3341298e35e19
quan <ashershirt@gmail.com>
2026-06-05 22:24:40 +0700

    fixx lỗi liên hệ

--- backend/test_client.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$request = Illuminate\Http\Request::create('/api/client/messages', 'GET');
-$request->headers->set('Authorization', 'Bearer 289|ZpIC0OeMTEKKv7PQbvajDBFIRX5skAvJMJZ3wEFCc3cde434');
-$response = app()->handle($request);
-echo $response->getStatusCode() . "\n";
-echo $response->getContent();
